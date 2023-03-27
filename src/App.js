668ef58ff13fffff25b02f1a1069b862c2fac6ae
ankitkdev <ankitrajpoot005@gmail.com>
2023-03-27 19:48:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -54,8 +54,6 @@ function App() {
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                 </div>
               </div>
-
-
             </div>
           )
         })}
